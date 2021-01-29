f701b98061d003db33d2ef833e97955c704f4850
Lena Hou <lenahou2013@gmail.com>
2021-01-29 22:33:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_string = ""
   array.each do |element|
-    new_string += "#{element.reverse()}"
+    new_string += "#{element.reverse()} "
 end
    return new_string
 end
