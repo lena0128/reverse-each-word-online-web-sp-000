8b4f03c5c5f8aca0ef44a0e566eb32c072a9f65e
Lena Hou <lenahou2013@gmail.com>
2021-01-29 23:09:21 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do |element|
+     new_array << element.reverse!
+end
+  new_array.join(" ")
+end
+
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |element|
+    element.reverse!
+  end
+  array.join(" ")
+end    
